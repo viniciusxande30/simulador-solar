f58f321a1ad5dc2c2603abba50f5d53e2371e8ad
viniciusxande30 <vinicius.xande30@gmail.com>
2023-01-30 15:39:44 -0300

    Update #1.1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -32,23 +32,77 @@ class Controller extends BaseController
       $email = $_POST['email'];
       $phone = $_POST['phone'];
       $state = $_POST['state'];
-      
+      $data_envio = date('d/m/Y');
+      $hora_envio = date('H:i:s');
+
+
+      $arquivo = "
+      <style type='text/css'>
+      body {
+      margin:0px;
+      font-family:Verdane;
+      font-size:12px;
+      color: #666666;
+      }
+      a{
+      color: #666666;
+      text-decoration: none;
+      }
+      a:hover {
+      color: #FF0000;
+      text-decoration: none;
+      }
+      </style>
+        <html>
+            <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+                <tr>
+                  <td>
+      <tr>
+                     <td width='500'>Nome:$name</td>
+                    </tr>
+                    <tr>
+                      <td width='320'>E-mail:<b>$email</b></td>
+         </tr>
+          <tr>
+                      <td width='320'>Telefone:<b>$phone</b></td>
+                    </tr>
+         <tr>
+                      <td width='320'>Opções:$state</td>
+                    </tr>
+                    
+                </td>
+              </tr>
+              <tr>
+                <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
+              </tr>
+            </table>
+        </html>
+      ";
+
+
       $emailenviar = "rsfreelas@gmail.com";
       $destino = $emailenviar;
-      $assunto = "Contato pelo Site";
+      $assunto = "Lead Simulador Solar";
 
-      $headers  = 'MIME-Version: 1.0' . "\r\n";
-          $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-          $headers .= "Olá " . $_POST["name"] . " email: " . $_POST["email"] . $_POST["state"] . $_POST["phone"] . ")<br><br>"."Mensagem para Você: ";;
-      //$headers .= "Bcc: $EmailPadrao\r\n";
+      // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+  $headers .= 'From: $nome <$email>';
 
-      $enviaremail = mail($destino, $assunto, $headers);
+      $enviaremail = mail($destino, $assunto, $arquivo, $headers);
       if($enviaremail){
         return view('result',$data);
       } else {
       $mgm = "ERRO AO ENVIAR E-MAIL!";
       echo "";
       }
+
+
+
+
+
+
+
 
 
 
