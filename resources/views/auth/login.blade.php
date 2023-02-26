490991feae83fb92858f758cb0598ee22348959b
viniciusxande30 <vinicius.xande30@gmail.com>
2023-02-26 20:07:36 -0300

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
+        <!-- <x-slot name="logo">
             <x-jet-authentication-card-logo />
-        </x-slot>
+        </x-slot> -->
 
         <x-jet-validation-errors class="mb-4" />
 
@@ -25,12 +25,12 @@
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <!-- <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
+            </div> -->
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
